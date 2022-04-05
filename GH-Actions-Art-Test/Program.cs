5dc6e0ebf2c28bf302d1457139bd02e74c2d7c15
ArsiaC01 <ethankoh364@gmail.com>
2022-04-05 09:03:52 -0400

    Added TesterProgram GH-Actions-Art-Test

---
+++ GH-Actions-Art-Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using GH_Actions_Art_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        // Main method for class.
+        static void Main()
+        {
+            // Declares an array of 10 Square objects.
+            Square[] squareArray = new Square[10];
+            // Creates 10 Square objects with values 1 through 10 and assigns them to the array.
+            for (int i = 0; i < squareArray.Length; i++)
+            {
+                squareArray[i] = new Square(i + 1);
+            }
+            // Display the values for each Square object in the array using for loop.
+            for (int i = 0; i < squareArray.Length; i++)
+            {
+                Console.WriteLine("Side = {0}, Area = {1}", squareArray[i].Side, squareArray[i].Area);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GH-Actions-Art-Test/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GH_Actions_Art_Test
+{
+    class Square
+    {
+        // Fields for area and length of side.
+        private readonly double side;
+        private readonly double area;
+
+        // Constructor for Square class that takes a side length as a parameter.
+        public Square(double side)
+        {
+            this.side = side;
+            this.area = side * side;
+        }
+        // Add read only properties to get a square side and area.
+        public double Side
+        {
+            get { return side; }
+        }
+        public double Area
+        {
+            get { return area; }
+        }
+    }
+}
